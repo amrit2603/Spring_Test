a26098a3225a7e256c0a9800e6ccadaf22170d84
amrit2603 <theamritkumar263@gmail.com>
2026-09-18 16:46:13 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/amrit/Module1Demo/Module1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.amrit.Module1Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1DemoApplication.class, args);
+	}
+
+}
