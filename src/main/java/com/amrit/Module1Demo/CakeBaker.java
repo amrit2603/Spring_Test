105e9ad321bc4230f36b31d43e7afc808ec1bdf6
amrit2603 <theamritkumar263@gmail.com>
2026-09-23 20:55:00 +0530

    completed CakeBaker class

--- src/main/java/com/amrit/Module1Demo/CakeBaker.java
+++ src/main/java/com/amrit/Module1Demo/CakeBaker.java
@@ -3,6 +3,6 @@ package com.amrit.Module1Demo;
 public class CakeBaker {
 
     public void bakeCake(){
-        System.out.println("Baking the cake ...");
+        System.out.println("Baking the cake in the house ...");
     }
 }
