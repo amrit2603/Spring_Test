dfc6d167f6b1a98ad6ecc9414c8e5b142f5e4311
amrit2603 <theamritkumar263@gmail.com>
2026-09-24 23:38:37 +0530

    Learned DeleteMapping

--- src/main/java/com/amrit/Module1Demo/CakeBaker.java
+++ src/main/java/com/amrit/Module1Demo/CakeBaker.java
@@ -3,6 +3,6 @@ package com.amrit.Module1Demo;
 public class CakeBaker {
 
     public void bakeCake(){
-        System.out.println("Baking the cAKE house hi..");
+        System.out.println("Baking the cake  house hi..");
     }
 }
