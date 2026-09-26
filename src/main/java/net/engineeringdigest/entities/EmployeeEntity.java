7669e614a24e76775352a7855adf65cc8fd6b8be
amrit2603 <theamritkumar263@gmail.com>
2026-09-26 23:22:04 +0530

    write EmployeeEntity

---
+++ src/main/java/net/engineeringdigest/entities/EmployeeEntity.java
@@ -0,0 +1,32 @@
+package net.engineeringdigest.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDateTime dateOfJoining;
+    @JsonProperty("isActive")
+    private boolean isActive;
+    private String role;
+    private Double salary;
+
+}
